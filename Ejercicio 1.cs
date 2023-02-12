8216d5ac82b9173ce3c58ca42546ccbf2ae0631a
PC <PC@DESKTOP-5OTN1FG>
2023-02-12 13:55:05 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio 1.cs
@@ -0,0 +1,4 @@
+﻿//Ejercicio 1: Nombre
+Console.WriteLine("Introduzca su nombre por favor");
+String nombre = Console.ReadLine(); 
+Console.WriteLine ("Su nombre es " + nombre);
---
+++ Ejercicio 2.cs
@@ -0,0 +1,4 @@
+﻿//Ejercicio 1: Nombre
+Console.WriteLine("Introduzca la hora actual por favor");
+String hora = Console.ReadLine(); 
+Console.WriteLine ("La hora actual en Argentina es " + hora);
